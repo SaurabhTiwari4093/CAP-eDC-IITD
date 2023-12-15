8a3a0622df9b32330faadfc9b81ac3eaf1458623
Saurabh Tiwari <86095513+SaurabhTiwari4093@users.noreply.github.com>
2023-12-15 21:17:04 +0530

    added promise in node mailer

--- routes/registration.js
+++ routes/registration.js
@@ -1,57 +1,56 @@
-const express = require('express')
-const router = express.Router()
-const Registration = require("../models/registration")
+const express = require("express");
+const router = express.Router();
+const Registration = require("../models/registration");
 
 // Confirmation mail
-var nodemailer = require('nodemailer');
-var smtpTransport = require('nodemailer-smtp-transport');
+var nodemailer = require("nodemailer");
+var smtpTransport = require("nodemailer-smtp-transport");
 
-var transporter = nodemailer.createTransport(smtpTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
+var transporter = nodemailer.createTransport(
+  smtpTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
     auth: {
-        user: 'saurabhtiwari4093@gmail.com',
-        pass: 'chihquflhkntihcm'
-    }
-}));
+      user: "saurabhtiwari4093@gmail.com",
+      pass: "chihquflhkntihcm",
+    },
+  })
+);
 
 //Get
-router.get('/', async (req, res) => {
-    try {
-        const registrations = await Registration.find()
-        res.status(200).json(
-            {
-                status: 200,
-                length:registrations.length,
-                registration: registrations
-            }
-        )
-    }
-    catch (err) {
-        res.status(500).json({
-            status: 500,
-            message: err.message
-        })
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const registrations = await Registration.find();
+    res.status(200).json({
+      status: 200,
+      length: registrations.length,
+      registration: registrations,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 500,
+      message: err.message,
+    });
+  }
+});
 
 //POST
-router.post('/', async (req, res) => {
-    const registration = new Registration({
-        name: req.body.name,
-        mobile: req.body.mobile,
-        email: req.body.email,
-        college: req.body.college,
-        question_1: req.body.question_1,
-        question_2: req.body.question_2,
-        question_3: req.body.question_3,
-    })
+router.post("/", async (req, res) => {
+  const registration = new Registration({
+    name: req.body.name,
+    mobile: req.body.mobile,
+    email: req.body.email,
+    college: req.body.college,
+    question_1: req.body.question_1,
+    question_2: req.body.question_2,
+    question_3: req.body.question_3,
+  });
 
-    var mailOptions = {
-        from: 'saurabhtiwari4093@gmail.com',
-        to: req.body.email,
-        subject: 'Successfully registered for CAP eDC IIT Delhi',
-        html: `
+  var mailOptions = {
+    from: "saurabhtiwari4093@gmail.com",
+    to: req.body.email,
+    subject: "Successfully registered for CAP eDC IIT Delhi",
+    html: `
         <p>
             Join this WhatsApp group for further notice<br>
             <a href="https://chat.whatsapp.com/Eu0lUBkKhP78ZeHPMiV0UT">https://chat.whatsapp.com/Eu0lUBkKhP78ZeHPMiV0UT</a>
@@ -87,31 +86,33 @@ router.post('/', async (req, res) => {
             Mention some of your work or experience that aligns with the CA program's purpose.<br>
             <i>${req.body.question_3}</i>
         </p>
-        `
-    };
+        `,
+  };
 
-    try {
-        const newRegistration = await registration.save()
-        res.status(201).json(
-            {
-                registration: newRegistration,
-                status: 201
-            }
-        )
-        transporter.sendMail(mailOptions, function (error, info) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('Email sent: ' + info.response);
-            }
-        });
-    }
-    catch (err) {
-        res.status(400).json({
-            status: 400,
-            message: err.message
-        })
-    }
-})
+  try {
+    const newRegistration = await registration.save();
+    res.status(201).json({
+      registration: newRegistration,
+      status: 201,
+    });
+    await new Promise((resolve, reject) => {
+      // send mail
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+          reject(err);
+        } else {
+          console.log("Email sent: " + info.response);
+          resolve(info.response);
+        }
+      });
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 400,
+      message: err.message,
+    });
+  }
+});
 
-module.exports = router
+module.exports = router;
